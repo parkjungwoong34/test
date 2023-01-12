264de79f0fb86779a1d4da2962272010836bd341
parkjungwoong34 <ppjjww1458@inha.edu>
2023-01-12 18:31:00 +0900

    github_test4

--- day01.py
+++ day01.py
@@ -1,12 +1,9 @@
-student = '김도훈'
-subject = '오래된 미래'
-print(student, '학생의 ', subject, '과목 성적은', subjects[subject], '입니다')
-# old style
-print("%s 학생의 %s 과목 성적은 %s 입니다"%(student, subject, subjects[subject]))
-# modern style (format함수)
-print("{0} 학생의 {1} 과목 성적은 {2}입니다".format(student, subject, subjects[subject]))
-# ultra modern style
-print (f'{student} 학생의 {subject} 과목 성적은 {subjects[subject]}입니다')
-#f스트링
-print(f'{student} 학생의 {subject} 과목 성적은 {subjects[subject]}입니다')
+# import tkinter
+
+import tkinter as tk
+
+win = tk.Tk()
+win. geometry('400x300')
+win. title('파이썬 1일차 Preview')
+win. mainloop()
 
