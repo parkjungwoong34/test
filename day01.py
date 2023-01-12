f78d877dfb1286dbd54705dec9bbe7ea8836d576
parkjungwoong34 <ppjjww1458@inha.edu>
2023-01-12 18:39:48 +0900

    github test1

--- day01.py
+++ day01.py
@@ -1,9 +1,4 @@
-# import tkinter
-
-import tkinter as tk
-
-win = tk.Tk()
-win. geometry('400x300')
-win. title('파이썬 1일차 Preview')
-win. mainloop()
-
+#(100°F − 32) × 5/9 = 37.778°C
+fahrenheit = float(input('화씨 온도 : '))
+celsius = (fahrenheit - 32.0) * (5.0/9.0)
+print(f'화씨 온도 {fahrenheit}도는 섭씨 온도 {celsius}입니다')
