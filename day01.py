f1fc9a2e2c25c93f1cded3954469ead6498bd4cb
parkjungwoong34 <ppjjww1458@inha.edu>
2023-01-12 22:16:34 +0900

    github_test4

--- day01.py
+++ day01.py
@@ -1,18 +1,4 @@
-# 교재 딕셔너리 예제 1-3
-
-subjects = {
-    '의사소통영어' : 'A+',
-    '오래된 미래' : 'B+',
-    '양자역학' : 'A0',
-}
-student = '김도훈'
-subject = '오래된 미래'
-print(student, '학생의 ', subject, '과목 성적은', subjects[subject], '입니다')
-# old style
-print("%s 학생의 %s 과목 성적은 %s 입니다"%(student, subject, subjects[subject]))
-# modern style (format함수)
-print("{0} 학생의 {1} 과목 성적은 {2}입니다".format(student, subject, subjects[subject]))
-# ultra modern style
-print (f'{student} 학생의 {subject} 과목 성적은 {subjects[subject]}입니다')
-#f스트링
-print(f'{student} 학생의 {subject} 과목 성적은 {subjects[subject]}입니다')
+#(100°F − 32) × 5/9 = 37.778°C
+fahrenheit = float(input('화씨 온도 : '))
+celsius = (fahrenheit - 32.0) * (5.0/9.0)
+print(f'화씨 온도 {fahrenheit}도는 섭씨 온도 {celsius}입니다')
