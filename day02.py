6a158ee31e1f666b95ede8b4e8448be1ad83dd2c
parkjungwoong34 <ppjjww1458@inha.edu>
2023-01-13 13:11:33 +0900

    day02_PI

--- day02.py
+++ day02.py
@@ -1,3 +1,4 @@
-harry_potter ={'헤르미온느' : '그리핀도르', '말포이' : '슬리데린', '론' : '그리핀도르'}
-print(harry_potter)
-print(type(harry_potter))
+math_value = [3.14, 2.71]
+print(f'원주율의 값은{PI}이고 타입은 {type(PI)}입니다')
+PI = 2.71
+print(f'원주율의 값은{math_values[0]이고 타입은 {type(mathe_values)}입니다')git
