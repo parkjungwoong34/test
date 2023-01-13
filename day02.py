8c8adbd7461dbc995a51c7ad22563a7b157b0dd5
parkjungwoong34 <ppjjww1458@inha.edu>
2023-01-13 15:26:22 +0900

    day02 random

--- day02.py
+++ day02.py
@@ -1,23 +1,9 @@
-# Chap 4
+import random
 
 limits = 20
-tweets = "pass" * 6
+tweets = "pass" * random.randint(1, 10) #1에서 10사이의 정수가 임의로 발생
 diff = limits - len(tweets)
-# if diff := limits - len(tweets) >= 0:
 if diff >= 0:
     print(tweets)
 else:
     print(f'글자 수 {abs(diff)} 초과')
-
-a = []
-print(bool(a))
-a.append(5)
-print(bool(a))
-print(bool(set()))
-print(bool(dict()))
-print(bool())
-
-vowels = 'aeiou'
-letter = 'u'
-if letter not in vowels:
-    print("실행 안됨!")
