a139b502f55897ab4552b6154a3d79f926f0d048
parkjungwoong34 <ppjjww1458@inha.edu>
2023-01-13 16:43:03 +0900

    test2

--- test.py
+++ test.py
@@ -1,11 +1,7 @@
-
-import random
-
-secret = random.randint(1, 10)
-guess = random.randint(1, 10)
-if secret - guess > 0:
-    print("too low")
-elif secret - guess < 0:
-    print("too high")
-else:
-    print("just right")
+small = True
+green = True
+if green:
+    if small:
+        print("It's pea")
+    else:
+        print("It`s watermelon")
